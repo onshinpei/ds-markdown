06e9760aedc19ff0fa14c772bec5fb6bbf3be951
onshinpei <244221001@qq.com>
2025-07-08 21:40:43 +0800

    add MarkdownProvider

--- src/context/MarkdownProvider.tsx
+++ src/context/MarkdownProvider.tsx
@@ -2,23 +2,16 @@ import React, { createContext, useContext, useMemo } from 'react';
 import type { MarkdownBaseProps } from '../defined';
 
 // 定义 Context 类型，允许部分属性
-export type MarkdownContextType = {
-  state: Partial<MarkdownBaseProps>;
-  // method
-};
+export type MarkdownContextType = Partial<MarkdownBaseProps>;
 
-const MarkdownContext = createContext<MarkdownContextType>(null!);
+const MarkdownContext = createContext<MarkdownContextType>({});
 
 export const MarkdownProvider: React.FC<{
   value: MarkdownContextType;
   children: React.ReactNode;
 }> = ({ value, children }) => {
-  const contextValue = useMemo(() => {
-    return {
-      state: value,
-    };
-  }, [value]);
-  return <MarkdownContext.Provider value={contextValue as MarkdownContextType}>{children}</MarkdownContext.Provider>;
+  const contextValue = useMemo(() => value, [value]);
+  return <MarkdownContext.Provider value={contextValue}>{children}</MarkdownContext.Provider>;
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
