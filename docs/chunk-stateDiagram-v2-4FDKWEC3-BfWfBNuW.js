82a51178e4c593e10d41926cc478c0fb780652d1
onshinpei <244221001@qq.com>
2025-11-24 21:51:29 +0800

    update website docs

---
+++ docs/chunk-stateDiagram-v2-4FDKWEC3-BfWfBNuW.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-chunk-DI55MBZ5-B7sivDtk.js";import{_ as i}from"./chunk-index-DflzO-yX.js";import"./chunk-chunk-55IACEB6-_ibndqm5.js";import"./chunk-chunk-QN33PNHL-Bn4xZhkl.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
